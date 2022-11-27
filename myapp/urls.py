e09419f52ef0a98eaaabae97516f9903f02df497
nurnobiCSE <shuvo517942@gmail.com>
2022-11-28 02:19:55 +0600

    insert and view complete

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('/show_employee_details/',views.View,name="view"),
+    path('/add_new_employee/',views.Add,name="add"),
+    path('/update_employee/',views.Edit,name="edit"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import *
 # Create your views here.
 def index(request):
+    return render(request,'index.html')
+
+def View(request):
     obj = Employee.objects.all()
     context = {
         "data": obj
     }
-    return render(request,'index.html',context)
+    return render(request,'view.html',context)
+
+def Add(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        title = request.POST.get('title')
+        salary = request.POST.get('salary')
+        branch = request.POST.get('branch')
+
+        store_data = Employee(
+            name = name,
+            email = email,
+            title = title,
+            salary = salary,
+            branch = branch
+        )
+        print(name)
+        store_data.save()
+        messages.success(request,"Employe added succesfully !")
+        return redirect('view')
+
+
+    return render(request,'insert.html')
+
+def Edit(request):
+    return render(request,'update.html')
